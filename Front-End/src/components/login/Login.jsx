3a961202887231e72df6e80e897377cd60833054
Riad zakaria <124533233+riadZk@users.noreply.github.com>
2023-12-08 23:46:42 +0100

    Update Login

--- Front-End/src/components/login/Login.jsx
+++ Front-End/src/components/login/Login.jsx
@@ -1,52 +1,43 @@
 import React, { useContext, useState, useRef } from 'react'
-import { useFormik } from 'formik';
 import { GymContext } from '../../context/context';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-import { ValidationLogin } from './ValidationLogin';
-
 export const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const { handleAuth } = useContext(GymContext);
+    
     const URL = process.env.REACT_APP_URL_BASE;
     const navigate = useNavigate('');
-    
-    const [email,setEmail] = useState('');
-    const [password,setPassword] = useState('');
 
-    const initialValues = {
-        email: '',
-        password: '',
-    }
-    console.log(initialValues)
-
-        const handleSubmit = async (e) => {
-            e.preventDefault();
-           const data = {
-            email:email,
-            password:password
-           }
-            try {
-                const response = await axios.post(`${URL}/login`, data);
-                console.log(response.data.data)
-                const authToken = response.data.data;
-                localStorage.setItem('authToken', authToken);
-                navigate('/');
-                window.location.reload(true);
-            } catch (error) {
-                console.error('Error sending message');
-            }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = {
+            email: email,
+            password: password
         }
-    
+        try {
+            const response = await axios.post(`${URL}/login`, data);
+            console.log(response.data.data)
+            const authToken = response.data.data;
+            localStorage.setItem('authToken', authToken);
+            navigate('/');
+            window.location.reload(true);
+        } catch (error) {
+            console.error('Error sending message');
+        }
+    }
+
     return (
         <div className='flex flex-col justify-center w-full md:w-1/2 h-full items-center gap-10'>
             <div className='text-center'>
                 <h1 className='text-link text-4xl font-bold mb-1'>Welcome back</h1>
                 <p className='text-gray-400'>Welcome back! Please enter your details.</p>
             </div>
-            <form className='w-2/3 max-[532px]:w-full px-2 md:px-0' 
-                  onSubmit={handleSubmit}
-                  >
+            <form className='w-2/3 max-[532px]:w-full px-2 md:px-0'
+                onSubmit={handleSubmit}
+            >
                 <div>
                     <div className='mb-5'>
                         <label className='text-light font-bold'>Email</label>
@@ -54,9 +45,9 @@ export const Login = () => {
                             type='text'
                             placeholder='Enter your email'
                             className='w-full p-3 rounded-lg text-black border-[.2px] border-black'
-                            onChange={(e)=> setEmail(e.target.value)}
+                            onChange={(e) => setEmail(e.target.value)}
                         />
-                          
+
                     </div>
                     <div className='mb-5'>
                         <label className='text-light font-bold'>Password</label>
@@ -64,7 +55,7 @@ export const Login = () => {
                             type='password'
                             placeholder='Enter your passwordl'
                             className='w-full p-3 rounded-lg text-black border-[.2px] border-black'
-                            onChange={(e)=> setPassword(e.target.value)}
+                            onChange={(e) => setPassword(e.target.value)}
                         />
 
 
